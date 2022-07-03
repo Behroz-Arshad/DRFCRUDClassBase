450200b08215c6a99440c92fae30b40e63892a36
M Behroz <behrozgujjar16@gmail.com>
2022-07-03 15:33:20 +0500

    feature-GenericApi-modelMixin

--- drfapp/urls.py
+++ drfapp/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from drfapp.views import Std
+from drfapp.views import Std, StudentListGenericMixinClass, StudentCreateGenericMixinClass, \
+    StudentDeleteGenericMixinClass ,StudentUpdateGenericMixinClass, StudentRetereveGenericMixinClass, \
+    DestroyModelMixin
 urlpatterns = [
-    path('',Std.as_view())
+    path('',Std.as_view()),
+    #generic api view and model mixin
+    path('student-list-mixin/', StudentListGenericMixinClass.as_view()),
+    path('student-create-mixin/', StudentCreateGenericMixinClass.as_view()),
+    path('student-update-mixin/<int:pk>/', StudentUpdateGenericMixinClass.as_view()),
+    path('student-delete-mixin/<int:pk>/', StudentDeleteGenericMixinClass.as_view()),
+    path('student-retreve-mixin/<int:pk>/', StudentRetereveGenericMixinClass.as_view())
 ]
--- drfapp/views.py
+++ drfapp/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from drfapp.models import Student
 from drfapp.serializer import StudentSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin, \
+    RetrieveModelMixin
 
 
 # Create your views here.
@@ -21,3 +24,44 @@ class Std(APIView):
             serializer.save()
             return Response({'msg': 'Created'}, status=status.HTTP_201_CREATED)
         return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Generic api view and model mixins
+class StudentListGenericMixinClass(GenericAPIView, ListModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class StudentCreateGenericMixinClass(GenericAPIView, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class StudentUpdateGenericMixinClass(GenericAPIView, UpdateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+
+class StudentDeleteGenericMixinClass(GenericAPIView, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class StudentRetereveGenericMixinClass(GenericAPIView, RetrieveModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
