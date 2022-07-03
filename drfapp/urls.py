35f752771c3b287650606a81532b1e37298ec9d0
M Behroz <behrozgujjar16@gmail.com>
2022-07-03 18:24:48 +0500

    GenericApi views like list view etc

--- drfapp/urls.py
+++ drfapp/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
-from drfapp.views import Std
+from drfapp.views import Std, StudentListAPIView, StudentCreateAPIView, StudentDestroyAPIView, StudentRetreveAPIView, \
+    StudentUpdateAPIView, StudentRetrieveDestroy, StudentRetrieveUpdateDestroy, StudentRetrieveUpdate
+
 urlpatterns = [
-    path('',Std.as_view())
+    path('', Std.as_view()),
+    path('StudentListAPIview/', StudentListAPIView.as_view()),
+    path('StudentCreateAPIView/', StudentCreateAPIView.as_view()),
+    path('StudentDestroyAPIView/<int:pk>/', StudentDestroyAPIView.as_view()),
+    path('StudentRetreveAPIView/<int:pk>/', StudentRetreveAPIView.as_view()),
+    path('StudentUpdateAPIView/<int:pk>/', StudentUpdateAPIView.as_view()),
+    # combination of more class to one
+    path('StudentRetrieveDestroyAPi/<int:pk>/', StudentRetrieveDestroy.as_view()),
+    path('StudentRetrieveUpdateDestroyudentAPi/<int:pk>/', StudentRetrieveUpdateDestroy.as_view()),
+    path('StudentRetrieveUpdateAPi/<int:pk>/', StudentRetrieveUpdate.as_view()),
+
 ]
--- drfapp/views.py
+++ drfapp/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from drfapp.models import Student
 from drfapp.serializer import StudentSerializer
+from rest_framework.generics import GenericAPIView, ListAPIView, ListCreateAPIView, UpdateAPIView, \
+    RetrieveUpdateAPIView, RetrieveUpdateDestroyAPIView, DestroyAPIView, RetrieveDestroyAPIView, \
+    CreateAPIView, RetrieveAPIView
 
 
 # Create your views here.
@@ -21,3 +24,49 @@ class Std(APIView):
             serializer.save()
             return Response({'msg': 'Created'}, status=status.HTTP_201_CREATED)
         return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# GenericAPi views Concerte API
+# This is mixture of GenericAPI and Model mixins
+
+class StudentListAPIView(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentCreateAPIView(CreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentUpdateAPIView(UpdateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentDestroyAPIView(DestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentRetreveAPIView(RetrieveAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+#combination of more class to 1 class
+
+
+class StudentRetrieveDestroy(RetrieveDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentRetrieveUpdate(RetrieveUpdateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
