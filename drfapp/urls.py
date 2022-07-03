e0105130f706ee821162b844ff9381c6bda08d94
M Behroz <behrozgujjar16@gmail.com>
2022-07-03 15:40:35 +0500

    Done complete CRUD in two urls

--- drfapp/urls.py
+++ drfapp/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from drfapp.views import Std, StudentListGenericMixinClass, StudentCreateGenericMixinClass, \
-    StudentDeleteGenericMixinClass ,StudentUpdateGenericMixinClass, StudentRetereveGenericMixinClass, \
-    DestroyModelMixin
+    StudentDeleteGenericMixinClass, StudentUpdateGenericMixinClass, StudentRetereveGenericMixinClass, \
+    StudentListAndCreateGenericMixinClass, StudentRUDGenericMixinClass
+
 urlpatterns = [
-    path('',Std.as_view()),
-    #generic api view and model mixin
+    path('', Std.as_view()),
+    # generic api view and model mixin
     path('student-list-mixin/', StudentListGenericMixinClass.as_view()),
     path('student-create-mixin/', StudentCreateGenericMixinClass.as_view()),
     path('student-update-mixin/<int:pk>/', StudentUpdateGenericMixinClass.as_view()),
     path('student-delete-mixin/<int:pk>/', StudentDeleteGenericMixinClass.as_view()),
-    path('student-retreve-mixin/<int:pk>/', StudentRetereveGenericMixinClass.as_view())
+    path('student-retreve-mixin/<int:pk>/', StudentRetereveGenericMixinClass.as_view()),
+
+    # generic api view and model mixin
+    path('student-list-create-mixin/', StudentListAndCreateGenericMixinClass.as_view()),
+    path('student-retreve-update-delete-mixin/<int:pk>/', StudentRUDGenericMixinClass.as_view()),
+
 ]
--- drfapp/views.py
+++ drfapp/views.py
@@ -65,3 +65,30 @@ class StudentRetereveGenericMixinClass(GenericAPIView, RetrieveModelMixin):
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
+
+
+#Generic Api views with model mixins but in a single class multiple functionalities
+
+class StudentRUDGenericMixinClass(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+
+class StudentListAndCreateGenericMixinClass(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
