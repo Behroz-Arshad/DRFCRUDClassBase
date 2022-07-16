9e7de91f3cefb9a9c5206a83857559b2035264bd
M Behroz <behrozgujjar16@gmail.com>
2022-07-16 20:43:07 +0500

    practice of viewset CRUD

--- drfapp/urls.py
+++ drfapp/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
-from drfapp.views import Std
+from django.urls import path, include
+from drfapp.views import Std, StudentViewset
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("studentviewset",StudentViewset, basename="student")
 urlpatterns = [
-    path('',Std.as_view())
+    path('',Std.as_view()),
+    path('', include(router.urls))
+
 ]
--- drfapp/views.py
+++ drfapp/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from drfapp.models import Student
 from drfapp.serializer import StudentSerializer
+from rest_framework.viewsets import ViewSet
 
 
 # Create your views here.
@@ -21,3 +22,52 @@ class Std(APIView):
             serializer.save()
             return Response({'msg': 'Created'}, status=status.HTTP_201_CREATED)
         return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+#Code for viewset
+
+class StudentViewset(ViewSet):
+    def list(self,request, *args, **kwargs):
+      student = Student.objects.all()
+      serializer = StudentSerializer(student, many=True)
+      return Response(serializer.data)
+
+    def create(self,request,*args,**kwargs):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg":"Create through viewset"}, status=status.HTTP_201_CREATED)
+        return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self,request,pk, *args, **kwargs):
+        if pk:
+            student_object= Student.objects.get(id=pk)
+            serializer = StudentSerializer(student_object, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"msg":"updated"},status=status.HTTP_200_OK)
+            return Response({"error":serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self,request, pk):
+        if pk:
+            student = Student.objects.get(id=pk)
+            serializer = StudentSerializer(student)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def partial_update(self,request,pk):
+        if pk:
+            student = Student.objects.get(id=pk)
+            serializer = StudentSerializer(student, data=request.data,partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"msg":"updated"}, status=status.HTTP_200_OK)
+            return Response({"errors":serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk):
+        if pk:
+            Student.objects.get(id=pk).delete()
+            return Response({"msg":"Deleted by viewset"},status=status.HTTP_200_OK)
+
+
+
+
+
